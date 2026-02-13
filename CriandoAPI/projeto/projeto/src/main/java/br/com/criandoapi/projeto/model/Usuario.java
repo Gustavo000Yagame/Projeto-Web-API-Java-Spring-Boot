925b55e01220d64255739548c35fdd5e9f7b89bd
🔐 Yagame <geraisusos25@gmail.com>
2026-02-13 00:39:40 -0300

    Add files via upload

---
+++ CriandoAPI/projeto/projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -0,0 +1,76 @@
+package br.com.criandoapi.projeto.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome_completo", length = 200, nullable = false)
+    private String nomeCompleto;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String senha;
+
+    @Column(length = 15, nullable = false)
+    private String telefone;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
